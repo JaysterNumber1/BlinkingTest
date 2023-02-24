72b6d885b6c867adbe899b2687a45b3fa89f5c52
Jayster CAPS <JaysterNumber1@gmail.com>
2023-02-24 14:44:38 -0700

    Got Item Finding Working, Jank Overlay system Howerver

--- Assets/Scripts/Search.cs
+++ Assets/Scripts/Search.cs
@@ -1,29 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class Search : MonoBehaviour
 {
     private GameObject[] database;
     private string[] list;
-    private InputField item;
-    private string itemName;
+    
+    
+    [SerializeField]
+    private TMP_InputField input;
+    [SerializeField]
+    private GameObject item;
+    [SerializeField]
+    private GameObject previousItem;
+    [SerializeField]
+    private Material foundMaterial;
+    [SerializeField]
+    private Material defaultMaterial;
+    [SerializeField]
+    private Camera itemCam;
    
     void Start()
     {
+        
         database = GameObject.FindGameObjectsWithTag("Medicine");
         list = new string[database.Length];
         for (int i = 0;i < database.Length; i++)
         {
             
 
-            list[i] = database[i].ToString();
-            Debug.Log(list[i]);
+            list[i] = database[i].name;
         }
-       
 
+        
     }
 
     // Update is called once per frame
@@ -37,13 +52,41 @@ public class Search : MonoBehaviour
 
     }
 
-    public void SearchList()
+    public void EndEdit()
     {
-        
-        if(list.Contains<string>())
+       
+        if (list.Contains(input.text))
         {
-            Debug.Log("ITEMFOUND");
+            int v = Array.IndexOf(list, input.text);
+            item = database[v];
+            Locate(item);
         }
+        else if (previousItem != null)
+        {
+            ClearLast(previousItem);
+        }
+       
     }
 
+    private void Locate(GameObject goal)
+    {
+        if (previousItem != null)
+        {
+            ClearLast(previousItem);
+        }
+        previousItem = goal;
+        goal.GetComponent<MeshRenderer>().material = foundMaterial;
+        goal.layer = 6;
+        itemCam.Render();
+       
+    }
+
+    private void ClearLast(GameObject previous)
+    {
+        previous.GetComponent<MeshRenderer>().material = defaultMaterial;
+        previous.layer = 0;
+
+    }
+
+   
 }
