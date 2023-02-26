6ad93038a120ab7698e72f0c622c33ce98fc7e86
Jayster <JaysterNumber1@gmail.com>
2023-02-25 21:17:07 -0700

    NavMesh, LineRenderer, etc.

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public Rigidbody player;
+    public Camera cam;
+    public float speed;
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+
+    public float h = 0f;
+    public float v = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a")){
+            player.velocity = -cam.transform.right * speed;
+        }
+        if (Input.GetKey("d")){
+            player.velocity = cam.transform.right * speed;
+        }
+        if (Input.GetKey("w")){
+            player.velocity = cam.transform.forward * speed;
+        }
+        if (Input.GetKey("s")){
+            player.velocity = -cam.transform.forward * speed;
+        }
+        player.velocity = new Vector3(player.velocity.x, 0, player.velocity.z);
+
+        h += horizontalSpeed* Input.GetAxis("Mouse X");
+         v -= verticalSpeed* Input.GetAxis("Mouse Y");
+
+      
+
+        cam.transform.eulerAngles = new Vector3(Mathf.Clamp(cam.transform.rotation.x+v, -50, 50), cam.transform.rotation.y+h, 0f);
+
+
+    }
+}
--- Assets/Scripts/Search.cs
+++ Assets/Scripts/Search.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
@@ -11,6 +12,10 @@ public class Search : MonoBehaviour
 {
     private GameObject[] database;
     private string[] list;
+    private GameObject currentItem;
+
+    public NavMeshAgent agent;
+    public LineRenderer line;
     
     
     [SerializeField]
@@ -24,10 +29,14 @@ public class Search : MonoBehaviour
     [SerializeField]
     private Material defaultMaterial;
     [SerializeField]
+    private Camera cam;
+    [SerializeField]
     private Camera itemCam;
    
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
+        line = GetComponent<LineRenderer>();
         
         database = GameObject.FindGameObjectsWithTag("Medicine");
         list = new string[database.Length];
@@ -36,15 +45,31 @@ public class Search : MonoBehaviour
             
 
             list[i] = database[i].name;
+            
+
         }
 
+        line.startWidth = .15f;
+        line.endWidth = .15f;
+        line.positionCount = 0;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentItem != null)
+        {
+            cam.transform.LookAt(currentItem.transform);
+        }
+        if(Vector3.Distance(agent.destination, transform.position) <= agent.stoppingDistance)
+        {
+            line.positionCount = 0;
+        }
+        else if (agent.hasPath)
+        {
+            DrawPath();
+        }
     }
    
     private void AutoFill()
@@ -70,14 +95,18 @@ public class Search : MonoBehaviour
 
     private void Locate(GameObject goal)
     {
+        
         if (previousItem != null)
         {
             ClearLast(previousItem);
         }
         previousItem = goal;
         goal.GetComponent<MeshRenderer>().material = foundMaterial;
+        currentItem = goal;
         goal.layer = 6;
         itemCam.Render();
+        agent.SetDestination(goal.transform.position);
+        
        
     }
 
@@ -86,6 +115,25 @@ public class Search : MonoBehaviour
         previous.GetComponent<MeshRenderer>().material = defaultMaterial;
         previous.layer = 0;
 
+    }
+
+    //Draws the path the camera follows to reach item
+
+    private void DrawPath()
+    {
+        line.positionCount = agent.path.corners.Length;
+        line.SetPosition(0, transform.position);
+
+        if (agent.path.corners.Length < 2)
+        {
+            return;
+        }
+
+        for(int i = 1; i < agent.path.corners.Length; i++)
+        {
+            Vector3 pointPosition = new Vector3(agent.path.corners[i].x, (currentItem.transform.position.y)*(i+1)/agent.path.corners.Length, agent.path.corners[i].z);
+            line.SetPosition(i, pointPosition);
+        }
     }
 
    
